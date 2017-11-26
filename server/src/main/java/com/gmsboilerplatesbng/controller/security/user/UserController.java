15977f90d49794e5cde6846d4de0600f0d061ba1
Asiel <lealceldeiro@gmail.com>
2017-11-26 17:36:40 -0500

    Server - Make sign-up and user "register" (through admin panel) two different actions. - Handle whether user registration is allowed or not

--- server/src/main/java/com/gmsboilerplatesbng/controller/security/user/UserController.java
+++ server/src/main/java/com/gmsboilerplatesbng/controller/security/user/UserController.java
@@ -2,6 +2,7 @@ package com.gmsboilerplatesbng.controller.security.user;
 
 import com.gmsboilerplatesbng.controller.BaseController;
 import com.gmsboilerplatesbng.domain.security.user.EUser;
+import com.gmsboilerplatesbng.service.configuration.ConfigurationService;
 import com.gmsboilerplatesbng.service.security.user.UserService;
 import com.gmsboilerplatesbng.util.exception.GmsGeneralException;
 import com.gmsboilerplatesbng.util.exception.domain.NotFoundEntityException;
@@ -19,21 +20,49 @@ public class UserController extends BaseController{
 
     private final UserService userService;
 
+    private final ConfigurationService configuration;
+
 
     @Autowired
-    public UserController(UserService userService, MessageResolver messageResolver) {
+    public UserController(UserService userService, MessageResolver messageResolver, ConfigurationService configuration) {
         this.userService = userService;
         this.msg = messageResolver;
+        this.configuration = configuration;
     }
 
+    /**
+     * Registers a new {@link EUser}
+     * @param user {@link EUser} data to be created
+     * @return A {@link EUser} mapped into a @{@link ResponseBody}
+     * @throws GmsGeneralException when an unhandled exception occurs
+     */
     @PostMapping("/")
     @ResponseStatus(HttpStatus.CREATED)
-    public @ResponseBody EUser signUp(@RequestBody EUser user) throws GmsGeneralException{
-        EUser u = this.userService.signUp(user);
+    public @ResponseBody EUser register(@RequestBody EUser user) throws GmsGeneralException {
+        return signUpUser(user, true);
+    }
+
+    /**
+     * Registers a new {@link EUser}
+     * @param user {@link EUser} data to be created
+     * @return A {@link EUser} mapped into a @{@link ResponseBody}
+     * @throws GmsGeneralException when an unhandled exception occurs
+     */
+    @PostMapping("/sign-up")
+    @ResponseStatus(HttpStatus.CREATED)
+    public @ResponseBody Object signUp(@RequestBody EUser user) throws GmsGeneralException {
+        if (this.configuration.isUserUserRegistrationAllowed()) {
+            return signUpUser(user, false);
+        }
+        else throw new GmsGeneralException("user.add.not_allowed", false);
+    }
+
+    private EUser signUpUser(EUser user, Boolean emailVerified) throws GmsGeneralException{
+        EUser u = this.userService.signUp(user, emailVerified);
         if (u != null) {
             return u;
         }
-        else throw new GmsGeneralException(this.msg.getMessage("exception.general"), false);
+        else throw new GmsGeneralException("user.add.error", false);
     }
 
     @PostMapping(value = "roles/add")
