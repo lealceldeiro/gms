f1ffac0c4663d8e6bffd1ed89c3c923f3f1d2998
Asiel <lealceldeiro@gmail.com>
2017-11-27 12:48:34 -0500

    Server: Add capability for specifying the authorities in the endpoint definition.

--- server/src/main/java/com/gmsboilerplatesbng/config/security/SecurityConfig.java
+++ server/src/main/java/com/gmsboilerplatesbng/config/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.security.Http401AuthenticationEntr
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,6 +18,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled=true, prePostEnabled=true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userDetailsService;
