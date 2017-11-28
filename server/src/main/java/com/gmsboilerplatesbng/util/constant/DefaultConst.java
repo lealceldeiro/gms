580d9c49211f620d44b09b0dc0c733db3f9c8018
Asiel <lealceldeiro@gmail.com>
2017-11-28 14:08:30 -0500

    Server: Make attributes final.

--- server/src/main/java/com/gmsboilerplatesbng/util/constant/DefaultConst.java
+++ server/src/main/java/com/gmsboilerplatesbng/util/constant/DefaultConst.java
@@ -6,46 +6,46 @@ public class DefaultConst {
 
     //region role
     @Value("${default.gmsrole.label}")
-    public static String ROLE_LABEL = "ROLE_ADMIN";
+    public static final String ROLE_LABEL = "ROLE_ADMIN";
 
     @Value("${default.gmsrole.description}")
-    public static String ROLE_DESCRIPTION = "Default role";
+    public static final String ROLE_DESCRIPTION = "Default role";
 
     @Value("${default.gmsrole.enabled}")
-    public static boolean ROLE_ENABLED = true;
+    public static final boolean ROLE_ENABLED = true;
     //endregion
 
     //region user
     @Value("${default.gmsuser.username}")
-    public static String USER_USERNAME = "admin";
+    public static final String USER_USERNAME = "admin";
 
     @Value("${default.gmsuser.email}")
-    public static String USER_EMAIL = "admin@example.com";
+    public static final String USER_EMAIL = "admin@example.com";
     //endregion
 
     //region entity
     @Value("${default.gmsentity.name}")
-    public static String ENTITY_NAME = "HOME";
+    public static final String ENTITY_NAME = "HOME";
 
     @Value("${default.gmsentity.username}")
-    public static String ENTITY_USERNAME = "home";
+    public static final String ENTITY_USERNAME = "home";
 
     @Value("${default.gmsentity.description}")
-    public static String ENTITY_DESCRIPTION = "Default entity";
+    public static final String ENTITY_DESCRIPTION = "Default entity";
     //endregion
 
     //region logic
     @Value("${default.entity.multi-entity}")
-    public static Boolean IS_MULTI_ENTITY = false;
+    public static final Boolean IS_MULTI_ENTITY = false;
 
     @Value("${default.user.registration.allowed}")
-    public static Boolean IS_USER_REGISTRATION_ALLOWED = true;
+    public static final Boolean IS_USER_REGISTRATION_ALLOWED = true;
     //endregion
 
     //region system
     @Value("${spring.data.rest.basePath}")
-    public static String API_BASE_PATH;
+    public static final String API_BASE_PATH = "/api";
 
-    public static String API_DOC_PATH = "/apidocs";
+    public static final String API_DOC_PATH = "/apidocs";
     //endregion
 }
