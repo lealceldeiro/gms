2cc6a6305debbc29bdc90cf7d923f3fa383ca3d1
Asiel <lealceldeiro@gmail.com>
2017-12-13 09:45:08 -0500

    Server: Some tweaks on attemptAuthentication method.

--- server/src/main/java/com/gmsboilerplatesbng/config/security/JWTAuthenticationFilter.java
+++ server/src/main/java/com/gmsboilerplatesbng/config/security/JWTAuthenticationFilter.java
@@ -42,6 +42,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     private final ObjectMapper oMapper;
 
+    @SuppressWarnings("all")
     @Override
     public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res) throws AuthenticationException {
         try {
