9cae6750b2f52f3a1643e296aac0b69ada9f18db
Asiel <lealceldeiro@gmail.com>
2017-12-13 16:19:59 -0500

    Server: Make attributes of DefaultConst private.

--- server/src/main/java/com/gmsboilerplatesbng/util/constant/DefaultConst.java
+++ server/src/main/java/com/gmsboilerplatesbng/util/constant/DefaultConst.java
@@ -1,5 +1,6 @@
 package com.gmsboilerplatesbng.util.constant;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -12,80 +13,81 @@ import org.springframework.stereotype.Component;
  * Dec 12, 2017
  */
 @Component
+@Getter
 public class DefaultConst {
 
     //region role
     @Value("${gms.role.default.label:ROLE_ADMIN}")
-    public String roleAdminDefaultLabel;
+    private String roleAdminDefaultLabel;
 
     @Value("${gms.role.default.description:Default role}")
-    public String roleAdminDefaultDescription;
+    private String roleAdminDefaultDescription;
 
     @Value("${gms.role.default.enabled:true}")
-    public boolean roleAdminDefaultEnabled;
+    private boolean roleAdminDefaultEnabled;
     //endregion
 
     //region user
     @Value("${gms.user.default.name:Admin}")
-    public String userAdminDefaultName;
+    private String userAdminDefaultName;
 
     @Value("${gms.user.default.lastName:Default}")
-    public String userAdminDefaultLastName;
+    private String userAdminDefaultLastName;
 
     @Value("${gms.user.default.username:admin}")
-    public String userAdminDefaultUsername;
+    private String userAdminDefaultUsername;
 
     @Value("${gms.user.default.password:admin}")
-    public String userAdminDefaultPassword;
+    private String userAdminDefaultPassword;
 
     @Value("${gms.user.default.email:admin@gms.com}")
-    public String userAdminDefaultEmail;
+    private String userAdminDefaultEmail;
     //endregion
 
     //region entity
     @Value("${gms.entity.default.name:HOME}")
-    public String entityDefaultName;
+    private String entityDefaultName;
 
     @Value("${gms.entity.default.username:home}")
-    public String entityDefaultUsername;
+    private String entityDefaultUsername;
 
     @Value("${gms.entity.default.description:Default entity}")
-    public String entityDefaultDescription;
+    private String entityDefaultDescription;
     //endregion
 
     //region config
     @Value("${gms.config.multi-entity:false}")
-    public Boolean isMultiEntity;
+    private Boolean isMultiEntity;
 
     @Value("${gms.config.user-registration-allowed:true}")
-    public Boolean isUserRegistrationAllowed;
+    private Boolean isUserRegistrationAllowed;
     //endregion
 
     //region system
     @Value("${spring.data.rest.basePath:/api}")
-    public String apiBasePath;
+    private String apiBasePath;
 
     @Value("${gms.config.api-docs.basePath:/apidocs}")
-    public String apiDocPath;
+    private String apiDocPath;
     //endregion
 
     //region vars
     @Value("${gms.config.page.size:1}")
-    public int pageSize;
+    private int pageSize;
 
     @Value("${gms.config.page.size_holder:size}")
-    public String pageSizeHolder;
+    private String pageSizeHolder;
 
     @Value("${gms.response.message:message}")
-    public String resMessageHolder;
+    private String resMessageHolder;
     //endregion
 
     //region lang
     @Value("${gms.i18n.lang_holder:lang}")
-    public String languageHolder;
+    private String languageHolder;
 
     @Value("${gms.i18n.default:en}")
-    public String defaultLanguage;
+    private String defaultLanguage;
 
     public static final String DEFAULT_LANGUAGE_HEADER = "Accept-Language";
     //endregion
