8684ab18ab3aa3077d53358c42f9c6013aef549e
Asiel <lealceldeiro@gmail.com>
2018-02-02 11:05:36 -0500

    Server: Update domain entity persistence validation.

--- server/src/test/java/com/gms/domain/configuration/BConfigurationTest.java
+++ server/src/test/java/com/gms/domain/configuration/BConfigurationTest.java
@@ -1,6 +1,7 @@
 package com.gms.domain.configuration;
 
 import com.gms.Application;
+import com.gms.util.validation.PersistenceValidation;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,6 +27,54 @@ public class BConfigurationTest {
     private final Long id = 1L;
     private BConfiguration entity;
     private BConfiguration entity1;
+    private final int MAX_RANGE_255 = 255;
+
+    //region persistence constraints validations
+    @Test
+    public void keyIsNotBlank() {
+        propertyIsNot("", v, "validation.field.notBlank", "key property must not be blank");
+    }
+
+    @Test
+    public void keyIsNotNull() {
+        propertyIsNot(null, v, "validation.field.notNull", "key property must not be null");
+    }
+
+    @Test
+    public void keyIsNotOutOfRange() {
+        StringBuilder testKeyMoreThanAllowedChars = new StringBuilder("a");
+        for (int i = 0; i < MAX_RANGE_255; i++) {
+            testKeyMoreThanAllowedChars.append(String.valueOf(i));
+        }
+        propertyIsNot(testKeyMoreThanAllowedChars.toString(), v, "validation.field.size",
+                "key property must not be of size less than 0 and more than 255 characters");
+    }
+
+    @Test
+    public void valueIsNotBlank() {
+        propertyIsNot(k, "", "validation.field.notBlank", "value property must not be blank");
+    }
+
+    @Test
+    public void valueIsNotNull() {
+        propertyIsNot(k, null, "validation.field.notNull", "value property must not be null");
+    }
+
+    @Test
+    public void valueIsNotOutOfRange() {
+        StringBuilder testKeyMoreThanAllowedChars = new StringBuilder("a");
+        for (int i = 0; i < MAX_RANGE_255; i++) {
+            testKeyMoreThanAllowedChars.append(String.valueOf(i));
+        }
+        propertyIsNot(k, testKeyMoreThanAllowedChars.toString(), "validation.field.size",
+                "value property must not be of size less than 0 and more than 255 characters");
+    }
+
+    public void propertyIsNot(String keyVal, String valueVal, String messageTests, String assertMessage) {
+        BConfiguration c = new BConfiguration(keyVal, valueVal);
+        assertTrue(assertMessage, PersistenceValidation.objectIsInvalidWithErrorMessage(c, messageTests));
+    }
+    //endregion
 
     @Test
     public void setKey() {
---
+++ server/src/test/java/com/gms/util/validation/PersistenceValidation.java
@@ -0,0 +1,51 @@
+package com.gms.util.validation;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * HibernateValidationUtil
+ *
+ * @author Asiel Leal Celdeiro | lealceldeiro@gmail.com
+ * @version 0.1
+ * Feb 02, 2018
+ */
+public class PersistenceValidation {
+    private static Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+    /**
+     * Test the validity of an object against the error messages the validator provides.
+     *
+     * @param o            Object to be tested.
+     * @param errorMessage Error message that must be found on the errors found when this object is invalid.
+     * @return <code>true</code> if the object is invalid and there is an error message equals to the one provided as third
+     * parameter. <code>false</code> otherwise.
+     */
+    public static boolean objectIsInvalidWithErrorMessage(Object o, String errorMessage) {
+        final Iterator<ConstraintViolation<Object>> cv = validate(o).iterator();
+        boolean ok = false;
+        ConstraintViolation<Object> v;
+        while (cv.hasNext() && !ok) {
+            v = cv.next();
+            if (v.getMessage().equals(errorMessage)) {
+                ok = true;
+            }
+        }
+        return ok;
+    }
+
+    public static Set<ConstraintViolation<Object>> validate(Object o, Class<?>... groups) {
+        return validator.validate(o, groups);
+    }
+
+    public static Set<ConstraintViolation<Object>> validateProperty(Object o, String propertyName, Class<?>... groups) {
+        return validator.validateProperty(o, propertyName, groups);
+    }
+
+    public static Set<ConstraintViolation<Object>> validateValue(Class<Object> beanType, String propertyName, Object value, Class<?>... groups) {
+        return validator.validateValue(beanType, propertyName, value, groups);
+    }
+}
