477fe474f92e541ca6a1ce1f5914017e96917e9f
Asiel <lealceldeiro@gmail.com>
2018-02-21 09:47:27 -0500

    Client: Update tests.

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -14,15 +14,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'GMS Boilerplate Client'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('GMS Boilerplate Client');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('GMS Boilerplate Client');
   }));
 });
