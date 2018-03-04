983d33e4e8bb0992b1ae2990f2ab0b7bc83707a5
Asiel <lealceldeiro@gmail.com>
2018-03-04 09:41:55 -0500

    Server: Update EUser JUnit test.  - Added invalid username (white spaces added) for checking correct behavior.

--- server/src/test/java/com/gms/util/StringUtil.java
+++ server/src/test/java/com/gms/util/StringUtil.java
@@ -26,6 +26,7 @@ public class StringUtil {
             "-randomChars",
             "_random_Chars",
             "_random-Chars",
+            "_random Chars whit spaces",
 
             "9randomChars9-",
             "randomChars9-",
