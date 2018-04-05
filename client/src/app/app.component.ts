262d8de963b8ccdb4124138db4d3c04f979065d2
Asiel <lealceldeiro@gmail.com>
2018-04-05 16:27:14 -0400

    Client: Change default to sass instead of css.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'gms-shell',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'GMS Boilerplate Client';
