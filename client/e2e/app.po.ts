142586ee24a97a581cab80dc493fb48b07edbe41
Asiel <lealceldeiro@gmail.com>
2018-04-05 14:06:36 -0400

    Client: Change ng app name.

--- client/e2e/app.po.ts
+++ client/e2e/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('gms-shell h1')).getText();
   }
 }
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'gms-shell',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
