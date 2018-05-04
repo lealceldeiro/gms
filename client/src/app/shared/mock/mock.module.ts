bfb8ffde76bc504ed4a07d50b3dfc1eb394cd12c
Asiel <lealceldeiro@gmail.com>
2018-05-04 16:41:10 -0400

    Client: Add mock module.

---
+++ client/src/app/shared/mock/mock.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { NgbCollapseStubDirective } from './ngb-collapse-stub.directive';
+import { RouterLinkStubDirective } from './router-link-stub.directive';
+
+@NgModule({
+  declarations: [ NgbCollapseStubDirective, RouterLinkStubDirective]
+})
+export class MockModule { }
