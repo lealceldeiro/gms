6bcb68a627f0445a4e2948e93ac8229fe022bf31
Asiel <lealceldeiro@gmail.com>
2018-05-07 15:26:52 -0400

    Client: Page not found component specs done.

--- client/src/app/page-not-found/page-not-found.component.spec.ts
+++ client/src/app/page-not-found/page-not-found.component.spec.ts
@@ -13,7 +13,7 @@ describe('PageNotFoundComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ PageNotFoundComponent ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -26,5 +26,21 @@ describe('PageNotFoundComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create with default values', () => {
+    fixture.whenRenderingDone().then(() => {
+      expect(componentEl.querySelector('span.firstDigit').textContent).toBe('4');
+      expect(componentEl.querySelector('span.secondDigit').textContent).toBe('0');
+      expect(componentEl.querySelector('span.thirdDigit').textContent).toBe('4');
+    });
+  });
+
+  it('should render `Sorry!` in a div', () => {
+    expect(componentEl.querySelector('div.msg').textContent).toContain('Sorry!');
+  });
+
+  it('should render `Page not found` in an <h2>', () => {
+    expect(componentEl.querySelector('h2').textContent).toBe('Page not found');
   });
 });
