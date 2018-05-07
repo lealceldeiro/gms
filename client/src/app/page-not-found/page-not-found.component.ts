11f139c442267313455b4b2318889996cff41637
Asiel <lealceldeiro@gmail.com>
2018-05-07 14:14:24 -0400

    Client: Updated not found component.

--- client/src/app/page-not-found/page-not-found.component.ts
+++ client/src/app/page-not-found/page-not-found.component.ts
@@ -11,14 +11,71 @@ import { Component, OnInit } from '@angular/core';
 export class PageNotFoundComponent implements OnInit {
 
   /**
+   * Third digit in the 404 text.
+   */
+  thirdDigit: string;
+
+  /**
+   * Second digit in the 404 text.
+   */
+  secondDigit: string;
+
+  /**
+   * First digit in the 404 text.
+   */
+  firstDigit: string;
+
+  /**
+   * Speed at which the random numbers will vary.
+   * @type {number}
+   */
+  private time = 30;
+
+  /**
+   * Generates a random number as string.
+   * @returns {string}
+   */
+  private static randomNumber(): string {
+    return (Math.floor(Math.random() * 9) + 1).toString();
+  }
+
+  /**
    * Component constructor
    */
   constructor() { }
 
   /**
-   * Lifecycle hook that is called after data-bound properties of a directive are initialized.
+   * Lifecycle hook that is called after data-bound properties are initialized.
    */
   ngOnInit() {
+    this.initialize404();
   }
 
+  initialize404(): void {
+    let i = 0;
+    const first4 = setInterval(() => {
+      if (i++ > 40) {
+        clearInterval(first4);
+        this.firstDigit = '4';
+      } else {
+        this.firstDigit = PageNotFoundComponent.randomNumber();
+      }
+    }, this.time);
+    const second4 = setInterval(() => {
+      if (i++ > 80) {
+        clearInterval(second4);
+        this.secondDigit = '0';
+      } else {
+        this.secondDigit = PageNotFoundComponent.randomNumber();
+      }
+    }, this.time);
+    const third4 = setInterval(() => {
+      if (i++ > 100) {
+        clearInterval(third4);
+        this.thirdDigit = '4';
+      } else {
+        this.thirdDigit = PageNotFoundComponent.randomNumber();
+      }
+    }, this.time);
+  }
 }
