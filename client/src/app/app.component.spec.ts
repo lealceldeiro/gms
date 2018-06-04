a69d23c4ab465df2fab9fe4d403647e431a6636e
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2018-06-04 09:49:15 -0400

    Client: Update tests for app.component

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,13 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Component } from '@angular/core';
 import { SessionService } from './core/session/session.service';
-import { of } from 'rxjs/index';
+import { of, Subject } from 'rxjs/index';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  const event = new Event('beforeunload', { cancelable: true });
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+  let isRememberMeSpy: jasmine.Spy;
+  let closeSessionSpy: jasmine.Spy;
 
   // region mocks
   @Component({selector: 'gms-nav-bar', template: ''})
@@ -19,9 +22,14 @@ describe('AppComponent', () => {
   @Component({selector: 'router-outlet', template: ''})  // tslint:disable-line
   class RouterOutletStubComponent {}
 
+  const spy = { isRememberMe: () => {}, closeSession: () => {} };
+  const subject = new Subject();
+
   const sessionServiceStub = {
     isNotLoggedIn: () => of(false),
     isLoggedIn: () => of(true),
+    isRememberMe: () => { spy.isRememberMe(); return subject.asObservable(); },
+    closeSession: () => { spy.closeSession(); }
   };
   // endregion
 
@@ -35,10 +43,26 @@ describe('AppComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
+    isRememberMeSpy = spyOn(spy, 'isRememberMe');
+    closeSessionSpy = spyOn(spy, 'closeSession');
     fixture.detectChanges();
   });
 
   it('should create the app', async(() => {
     expect(component).toBeTruthy();
   }));
+
+  it('should call SessionService#isRememberMe in order to get to know whether the session data should be kept ' +
+    'or not ("remember me" is true)', () => {
+    window.dispatchEvent(event);
+    expect(isRememberMeSpy).toHaveBeenCalled();
+  });
+
+  it('should call SessionService#isRememberMe in order to get to know whether the session data should be kept ' +
+    'or not ("remember me" is false)', () => {
+    window.dispatchEvent(event);
+    expect(isRememberMeSpy).toHaveBeenCalledTimes(1);
+    subject.next(false); // do not delete data
+    expect(closeSessionSpy).toHaveBeenCalled();
+  });
 });
