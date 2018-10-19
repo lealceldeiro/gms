6bb1b92c746c98061f8461e7bb11cb8124bd7d3a
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2018-10-19 14:16:27 -0400

    Client: Update dev API url.

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -10,7 +10,7 @@
 export const environment = {
   production: false,
 
-  apiBaseUrl: 'http://127.0.0.1:8081/gms/api/',
+  apiBaseUrl: 'http://127.0.0.1:8081/api/',
   apiLoginUrl: 'login',
   metaName: 'General Management System'
 };
