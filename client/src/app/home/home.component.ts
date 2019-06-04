3d9022735415fad6de9f57baeec7d9f7af4e2550
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2019-06-04 14:54:15 +0300

    Client: Update home comp: show jumbotron if logged

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { SessionService } from '../core/session/session.service';
+import { Subscription } from 'rxjs';
 
 /**
  * Component for generating the home page of the app.
@@ -9,7 +11,7 @@ import { environment } from '../../environments/environment';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   /**
    * Application name.
@@ -18,13 +20,34 @@ export class HomeComponent implements OnInit {
   appName: string = environment.metaName;
 
   /**
+   * Indicates whether the user is logged in or not.
+   * @type {boolean}
+   */
+  isLoggedIn = false;
+
+  /**
+   * Subscription to the logged in status.
+   * @type {Subscription}
+   */
+  isLoggedInSubscription: Subscription;
+
+  /**
    * Component constructor
    */
-  constructor() { }
+  constructor(private sessionService: SessionService) { }
 
   /**
    * Lifecycle hook that is called after data-bound properties are initialized.
    */
-  ngOnInit() { }
+  ngOnInit() {
+    this.isLoggedInSubscription = this.sessionService.isLoggedIn().subscribe(ili => this.isLoggedIn = ili);
+  }
+
+  /**
+   * Lifecycle hook that is called hen the component is destroyed.
+   */
+  ngOnDestroy() {
+    this.isLoggedInSubscription.unsubscribe();
+  }
 
 }
