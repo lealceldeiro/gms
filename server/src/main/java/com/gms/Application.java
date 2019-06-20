6a9ff93e3b177323c52ca45591814a7aa26f711a
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2019-06-20 11:28:35 +0300

    Server: Remove unnecesary apidocs and format code.

--- server/src/main/java/com/gms/Application.java
+++ server/src/main/java/com/gms/Application.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-
 /**
  * Entry point to the application.
  *
