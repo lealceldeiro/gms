c7fc6747876aecb67dbe3ec8b42ea9f9ef24dd4a
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2019-06-21 10:22:47 +0300

    Client: Add remaining tests

--- client/src/app/permissions/permission-info/permission-info.component.spec.ts
+++ client/src/app/permissions/permission-info/permission-info.component.spec.ts
@@ -1,22 +1,28 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Location } from '@angular/common';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { getRandomNumber } from '../../shared/test-util/functions.util';
-import { PermissionInfoComponent } from './permission-info.component';
-import { PermissionService } from '../shared/permission.service';
 import { Permission } from '../shared/permission.model';
+import { PermissionService } from '../shared/permission.service';
+import { PermissionInfoComponent } from './permission-info.component';
 
 describe('PermissionInfoComponent', () => {
   let component: PermissionInfoComponent;
   let fixture: ComponentFixture<PermissionInfoComponent>;
+  const spy = { location: { back: () => { } }, permission: { getInfo: (_id: any) => { } } };
   const permissionServiceStub = {
-    getPermissionInfo: (_id: number): Observable<Permission> => new BehaviorSubject<Permission>(new Permission()).asObservable()
+    getPermissionInfo: (_id: number): Observable<Permission> => {
+      spy.permission.getInfo(_id);
+      return new BehaviorSubject<Permission>(new Permission()).asObservable();
+    }
   };
-  const locationStub = { back: () => { } };
+  const locationStub = { back: () => { spy.location.back(); } };
   const id = getRandomNumber();
-  const activatedRouteStub = { snapshot: { paramMap: { get: (): any => id } } };
+  let idGetter = (): any => id;
+  const activatedRouteStub = { snapshot: { paramMap: { get: (): any => idGetter() } } };
   let getPermissionInfoSpy: jasmine.Spy;
+  let locationBackSpy: jasmine.Spy;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -30,8 +36,7 @@ describe('PermissionInfoComponent', () => {
   }));
 
   beforeEach(() => {
-    activatedRouteStub.snapshot.paramMap.get = () => id;
-    getPermissionInfoSpy = spyOn(permissionServiceStub, 'getPermissionInfo');
+    idGetter = (): any => id;
     fixture = TestBed.createComponent(PermissionInfoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -40,24 +45,27 @@ describe('PermissionInfoComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-/*
+
   it('should call PermissionService#getPermissionInfo on init', () => {
+    getPermissionInfoSpy = spyOn(spy.permission, 'getInfo');
+    component.ngOnInit();
     expect(getPermissionInfoSpy).toHaveBeenCalledTimes(1);
     expect(getPermissionInfoSpy).toHaveBeenCalledWith(id);
   });
 
   it('should not call PermissionService#getPermissionInfo, but Location#back if the id is an invalid value', () => {
-    activatedRouteStub.snapshot.paramMap.get = () => 'someInvalidId';
-    const locationBackSpy = spyOn(locationStub, 'back');
+    getPermissionInfoSpy = spyOn(spy.permission, 'getInfo');
+    locationBackSpy = spyOn(spy.location, 'back');
+    idGetter = (): any => 'someInvalidId';
     component['getPermissionInfo']();
     expect(getPermissionInfoSpy).not.toHaveBeenCalled();
     expect(locationBackSpy).toHaveBeenCalledTimes(1);
   });
 
   it('should call Location#back on call to back function', () => {
-    const locationBackSpy = spyOn(locationStub, 'back');
+    locationBackSpy = spyOn(spy.location, 'back');
     component['back']();
     expect(locationBackSpy).toHaveBeenCalledTimes(1);
   });
-  */
+
 });
