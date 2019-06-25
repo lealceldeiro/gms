c19a51248b21634930f70a6fc23c83e6e9b5fe97
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2019-06-25 11:58:21 +0300

    Server: update imports

--- server/src/main/java/com/gms/controller/security/ownedentity/RestOwnedEntityController.java
+++ server/src/main/java/com/gms/controller/security/ownedentity/RestOwnedEntityController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import lombok.RequiredArgsConstructor;
 
