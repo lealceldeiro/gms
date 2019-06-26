6073c410c6e223ea03a546d0dfc869027b0668d9
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2019-06-26 10:00:45 +0300

    Client: Move BS component exportation to BS module

--- client/src/app/shared/bootstrap-components/bootstrap-components.module.ts
+++ client/src/app/shared/bootstrap-components/bootstrap-components.module.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule, NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 import { GmsPaginationComponent } from './gms-pagination/gms-pagination.component';
 import { GmsJumbotronComponent } from './jumbotron/jumbotron.component';
 import { PreviewContentComponent } from './preview-content/preview-content.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, NgbModule],
+  imports: [CommonModule, RouterModule, NgbModule, NgbCollapseModule],
   declarations: [GmsJumbotronComponent, PreviewContentComponent, GmsPaginationComponent],
-  exports: [GmsJumbotronComponent, PreviewContentComponent, GmsPaginationComponent]
+  exports: [NgbModule, NgbCollapseModule, GmsJumbotronComponent, PreviewContentComponent, GmsPaginationComponent]
 })
 export class GmsBootstrapComponentsModule { }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgbCollapseModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { GmsBootstrapComponentsModule } from './bootstrap-components/bootstrap-components.module';
 import { BooleanPipe } from './boolean-pipe/boolean.pipe';
+import { GmsBootstrapComponentsModule } from './bootstrap-components/bootstrap-components.module';
 
 @NgModule({
   imports: [],
   declarations: [BooleanPipe],
-  exports: [CommonModule, FormsModule, ReactiveFormsModule, GmsBootstrapComponentsModule, NgbCollapseModule, NgbModule, BooleanPipe]
+  exports: [CommonModule, FormsModule, ReactiveFormsModule, GmsBootstrapComponentsModule, BooleanPipe]
 })
 export class SharedModule { }
