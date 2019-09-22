bab51fed8e2ccb94d3b4f9f5682762a2bc604614
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2019-09-22 11:01:39 +0300

    Client: Add service for getting the user's config.

--- client/src/app/configurations/shared/configuration.service.spec.ts
+++ client/src/app/configurations/shared/configuration.service.spec.ts
@@ -2,22 +2,34 @@ import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { environment } from 'src/environments/environment';
 import { ConfigurationService } from './configuration.service';
+import { SessionService } from 'src/app/core/session/session.service';
+import { BehaviorSubject } from 'rxjs';
+import { User } from 'src/app/core/session/user.model';
+import { getRandomNumber } from 'src/app/shared/test-util/functions.util';
 
 describe('ConfigurationService', () => {
-  const spy = { get: (_a: any, _b: any) => { } };
-  const httpClientMock = { get: (a: any, b: any) => spy.get(a, b) };
+  const httpSpy = { get: (_a: any, _b: any) => { } };
+  const sessionSSpy = { getUser: () => { } };
+  const httpClientMock = { get: (a: any, b: any) => httpSpy.get(a, b) };
+  const userBehaviorSubject = new BehaviorSubject<User>(new User());
+  const sessionServiceMock = {
+    getUser: () => { sessionSSpy.getUser(); return userBehaviorSubject.asObservable(); }
+  };
   let configurationService: ConfigurationService;
   let httpClientGetSpy: jasmine.Spy;
+  let sessionServiceSpy: jasmine.Spy;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         ConfigurationService,
-        { provide: HttpClient, useValue: httpClientMock }
+        { provide: HttpClient, useValue: httpClientMock },
+        { provide: SessionService, useValue: sessionServiceMock }
       ]
     });
     configurationService = TestBed.get(ConfigurationService);
-    httpClientGetSpy = spyOn(spy, 'get');
+    httpClientGetSpy = spyOn(httpSpy, 'get');
+    sessionServiceSpy = spyOn(sessionSSpy, 'getUser');
   });
 
   it('should be created', () => {
@@ -25,9 +37,22 @@ describe('ConfigurationService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('#getPermissions should call HttpClient#get with the proper parameters, retrieved from ParamsService#getHttpParams', () => {
+  it('#getConfigurations should call HttpClient#get', () => {
     configurationService.getConfigurations();
     expect(httpClientGetSpy).toHaveBeenCalledTimes(1);
     expect(httpClientGetSpy).toHaveBeenCalledWith(environment.apiBaseUrl + 'configuration', undefined);
+  });
+
+  it('#getUserConfigurations should call SessionService#getUser to get the user id and HttpClient#get', () => {
+    const sub = configurationService.getUserConfigurations().subscribe(() => {
+      expect(sessionServiceSpy).toHaveBeenCalledTimes(1);
+      expect(httpClientGetSpy).toHaveBeenCalledTimes(1);
+      expect(httpClientGetSpy).toHaveBeenCalledWith(`${environment.apiBaseUrl}configuration/${id}`, undefined);
+      sub.unsubscribe();
+    });
+    const id = getRandomNumber();
+    const user = new User();
+    user.id = id;
+    userBehaviorSubject.next(user);
   });
 });
--- client/src/app/configurations/shared/configuration.service.ts
+++ client/src/app/configurations/shared/configuration.service.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
+import { SessionService } from 'src/app/core/session/session.service';
+import { User } from 'src/app/core/session/user.model';
 
 /**
  * Service for providing configurations-related services.
@@ -18,7 +21,7 @@ export class ConfigurationService {
    * Service's constructor
    * @param {HttpClient} http HttpClient for performing api requests.
    */
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private readonly sessionService: SessionService) { }
 
   /**
    * Return an Observable of objects. Each object contains all system-wide available configurations.
@@ -26,5 +29,15 @@ export class ConfigurationService {
    */
   getConfigurations(): Observable<object> {
     return this.http.get<object>(this.url);
+  }
+
+  /**
+   * Return an Observable of objects. Each object contains the user available configurations.
+   * The configurations are in the shape o {configKey: configValue}.
+   */
+  getUserConfigurations(): Observable<object> {
+    return this.sessionService.getUser().pipe(
+      switchMap((user: User) => this.http.get<object>(`${this.url}/${user.id}`))
+    );
   }
 }
