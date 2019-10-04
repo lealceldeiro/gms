d0c298f259e6a14d6c0df5810ff6b478035fa144
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2019-10-04 16:34:53 +0300

    Client: Update Unit Test
    - Refactor unit tests

--- client/src/app/configurations/configuration-list/configuration-list.component.spec.ts
+++ client/src/app/configurations/configuration-list/configuration-list.component.spec.ts
@@ -5,18 +5,15 @@ import { ConfigurationService } from '../shared/configuration.service';
 import { ConfigurationListComponent } from './configuration-list.component';
 
 describe('ConfigurationListComponent', () => {
-  let component: ConfigurationListComponent;
-  let fixture: ComponentFixture<ConfigurationListComponent>;
   const configurationObject: { [key: string]: string } = {};
   const numOfProps = getRandomNumber();
+  let component: ConfigurationListComponent;
+  let fixture: ComponentFixture<ConfigurationListComponent>;
+  let configurationServiceSpy: jasmine.SpyObj<ConfigurationService>;
 
   for (let i = 0; i < numOfProps; i++) {
     configurationObject['randomKey-' + getRandomNumber() + '-' + i] = 'randomValue-' + getRandomNumber() + '-' + i;
   }
-
-  let configurationServiceSpy: jasmine.SpyObj<ConfigurationService>;
-  let loadConfigurationsSpy: jasmine.Spy;
-  let loadUserConfigurationsSpy: jasmine.Spy;
 
   beforeEach(async(() => {
     configurationServiceSpy = jasmine.createSpyObj('ConfigurationService', ['getConfigurations', 'getUserConfigurations']);
@@ -33,8 +30,6 @@ describe('ConfigurationListComponent', () => {
     fixture = TestBed.createComponent(ConfigurationListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-    loadConfigurationsSpy = spyOn(component, 'loadConfigurations');
-    loadUserConfigurationsSpy = spyOn(component, 'loadUserConfigurations');
   });
 
   it('should create', () => {
@@ -42,21 +37,18 @@ describe('ConfigurationListComponent', () => {
   });
 
   it('should call loadConfigurations on init', () => {
+    const loadConfigurationsSpy = spyOn(component, 'loadConfigurations');
     component.ngOnInit();
     expect(loadConfigurationsSpy).toHaveBeenCalledTimes(1);
   });
 
   it('should call ConfigurationService#getConfigurations on call to loadConfigurations and set values from the observable ', () => {
-    component.loadConfigurations();
-    expect(loadConfigurationsSpy).toHaveBeenCalledTimes(1);
     expect(configurationServiceSpy.getConfigurations).toHaveBeenCalledTimes(1);
     expect(component.keys.system).toEqual(Object.keys(configurationObject));
     expect(component.values.system).toEqual(Object.values(configurationObject));
   });
 
   it('should call ConfigurationService#getUserConfigurations on call to loadUserConfigurations and set values from the observable ', () => {
-    component.loadUserConfigurations();
-    expect(loadUserConfigurationsSpy).toHaveBeenCalledTimes(1);
     expect(configurationServiceSpy.getUserConfigurations).toHaveBeenCalledTimes(1);
     expect(component.keys.user).toEqual(Object.keys(configurationObject));
     expect(component.values.user).toEqual(Object.values(configurationObject));
