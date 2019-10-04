0f81dbcc88d52da5e76dcd99fd94b16bad92f313
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2019-10-04 17:51:03 +0300

    Client: Update Unit Test
    - Refactor unit tests
    - Refactor import indentation

--- client/src/app/core/interceptor/all-requests.interceptor.spec.ts
+++ client/src/app/core/interceptor/all-requests.interceptor.spec.ts
@@ -2,37 +2,30 @@ import { HttpClient, HttpErrorResponse, HTTP_INTERCEPTORS } from '@angular/commo
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
+
 import { HttpStatusCode } from '../response/http-status-code.enum';
 import { AllRequestsInterceptor } from './all-requests.interceptor';
 
-
 describe('AllRequestsInterceptor', () => {
-
   const url = 'sample-url';
-  const spy = { startLoader: () => { }, stopLoader: () => { } };
-  const loaderServiceMock = { start: () => { spy.startLoader(); }, stopAll: () => { stopLoaderSpy(); } };
-
+  let loaderServiceSpy: jasmine.SpyObj<NgxUiLoaderService>;
   let httpTestingController: HttpTestingController;
   let httpClient: HttpClient;
-  let interceptor: AllRequestsInterceptor;
-  let startLoaderSpy: jasmine.Spy;
-  let stopLoaderSpy: jasmine.Spy;
 
   beforeEach(() => {
+    loaderServiceSpy = jasmine.createSpyObj('NgxUiLoaderService', ['start', 'stopAll']);
+
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [
         AllRequestsInterceptor,
         { provide: HTTP_INTERCEPTORS, useClass: AllRequestsInterceptor, multi: true },
-        { provide: NgxUiLoaderService, useValue: loaderServiceMock },
+        { provide: NgxUiLoaderService, useValue: loaderServiceSpy },
       ]
     });
 
     httpTestingController = TestBed.get(HttpTestingController);
     httpClient = TestBed.get(HttpClient);
-    interceptor = TestBed.get(AllRequestsInterceptor);
-    startLoaderSpy = spyOn(spy, 'startLoader');
-    stopLoaderSpy = spyOn(spy, 'stopLoader');
   });
 
   afterEach(() => {
@@ -46,13 +39,13 @@ describe('AllRequestsInterceptor', () => {
   });
 
   it('should call <loader_service>#start when the loader is not started and a request is performed', () => {
-    httpClient.get(url).subscribe(() => expect(startLoaderSpy).toHaveBeenCalledTimes(1));
+    httpClient.get(url).subscribe(() => expect(loaderServiceSpy.start).toHaveBeenCalledTimes(1));
     httpTestingController.expectOne(url).flush({});
   });
 
   it('should NOT call <loader_service>#start when the loader is already started and a request is performed', () => {
-    httpClient.get(url).subscribe(() => expect(startLoaderSpy).toHaveBeenCalledTimes(1));
-    httpClient.get(url).subscribe(() => expect(startLoaderSpy).not.toHaveBeenCalledTimes(2));
+    httpClient.get(url).subscribe(() => expect(loaderServiceSpy.start).toHaveBeenCalledTimes(1));
+    httpClient.get(url).subscribe(() => expect(loaderServiceSpy.start).not.toHaveBeenCalledTimes(2));
     const req = httpTestingController.match(url);
     req[0].flush({});
     req[1].flush({});
@@ -61,8 +54,8 @@ describe('AllRequestsInterceptor', () => {
   it('should call <loader_service>#stopAll when the <loader_service>#start has been called previously and a successful response arrived',
     () => {
       httpClient.get(url).subscribe(() => {
-        expect(startLoaderSpy).toHaveBeenCalledTimes(1);
-        expect(stopLoaderSpy).toHaveBeenCalledTimes(1);
+        expect(loaderServiceSpy.start).toHaveBeenCalledTimes(1);
+        expect(loaderServiceSpy.stopAll).toHaveBeenCalledTimes(1);
       });
       httpTestingController.expectOne(url).flush({});
     });
@@ -72,8 +65,8 @@ describe('AllRequestsInterceptor', () => {
       const error: HttpErrorResponse = new HttpErrorResponse({ status: HttpStatusCode.UNAUTHORIZED, statusText: 'Server Error', url: url });
       httpClient.get(url).subscribe(() => { }, () => {
         // error
-        expect(startLoaderSpy).toHaveBeenCalledTimes(1);
-        expect(stopLoaderSpy).toHaveBeenCalledTimes(1);
+        expect(loaderServiceSpy.start).toHaveBeenCalledTimes(1);
+        expect(loaderServiceSpy.stopAll).toHaveBeenCalledTimes(1);
       });
       httpTestingController.expectOne(url).flush({}, error);
     });
