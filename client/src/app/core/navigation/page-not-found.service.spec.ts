51f9a894c3e9dc69438bd7f6f2e411f1a9ebeb10
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2019-10-04 21:48:56 +0300

    Client: Update Unit Test
    - Refactor unit tests

--- client/src/app/core/navigation/page-not-found.service.spec.ts
+++ client/src/app/core/navigation/page-not-found.service.spec.ts
@@ -1,10 +1,11 @@
 import { inject, TestBed } from '@angular/core/testing';
+
 import { PageNotFoundService } from './page-not-found.service';
 
 describe('PageNotFoundService', () => {
   const url = '/test';
   let service: PageNotFoundService;
-  let collection: Array<String> = [];
+  let collection: Array<string> = [];
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -19,36 +20,36 @@ describe('PageNotFoundService', () => {
   }));
 
   it('#removeUrl should remove the url from the collection when it exist', () => {
-    expect(collection.indexOf(url)).toBe(-1, 'Collection had the value already');
+    expect(collection).not.toContain(url, 'Collection had the value already');
     collection.push(url);
-    expect(collection.indexOf(url)).not.toBe(-1, 'Collection does not have the value');
+    expect(collection).toContain(url, 'Collection does not have the value');
     service.removeUrl(url);
-    expect(collection.indexOf(url)).toBe(-1, 'Collection did not remove the value');
+    expect(collection).not.toContain(url, 'Collection did not remove the value');
   });
 
   it('#removeUrl should do nothing if the url is not in the collection', () => {
-    expect(collection.indexOf(url)).toBe(-1, 'Collection had the value already');
+    expect(collection).not.toContain(url, 'Collection had the value already');
     service.removeUrl(url);
-    expect(collection.indexOf(url)).toBe(-1, 'Collection did not remove the value');
+    expect(collection).not.toContain(url, 'Collection should not have the value');
   });
 
   it('#addUrl should add the url to the collection when the url is not in the collection already', () => {
-    expect(collection.indexOf(url)).toBe(-1, 'Collection had the value already');
+    expect(collection).not.toContain(url, 'Collection had the value already');
     service.addUrl(url);
-    expect(collection.indexOf(url)).not.toBe(-1, 'Collection did not add the value');
+    expect(collection).toContain(url, 'Collection did not add the value');
   });
 
   it('#addUrl should do nothing when the url is in the collection already', () => {
-    expect(collection.indexOf(url)).toBe(-1, 'Collection had the value already');
+    expect(collection).not.toContain(url, 'Collection had the value already');
     collection.push(url);
-    expect(collection.indexOf(url)).not.toBe(-1, 'Collection did not add the value');
+    expect(collection).toContain(url, 'Collection did not add the value');
     service.addUrl(url);
-    expect(collection.lastIndexOf(url)).toBe(0, 'it added the value despite it was already' +
-      'in the collection. Collection is ' + JSON.stringify(collection));
+    expect(collection.lastIndexOf(url))
+      .toBe(0, `it added the value despite it was already in the collection. Collection is ${JSON.stringify(collection)}`);
   });
 
   it('#wasNotFound should return `true` if the url is in the collection', () => {
-    expect(collection.indexOf(url)).toBe(-1, 'Collection had the value already');
+    expect(collection).not.toContain(url, 'Collection had the value already');
     collection.push(url);
     expect(service.wasNotFound(url)).toBeTruthy();
   });
