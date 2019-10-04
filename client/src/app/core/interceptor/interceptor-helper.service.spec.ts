1ca9e2b47c62855ab00fdb23c85cfd3c653ffadd
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2019-10-04 18:21:15 +0300

    Client: Update Unit Test
    - Refactor unit tests

--- client/src/app/core/interceptor/interceptor-helper.service.spec.ts
+++ client/src/app/core/interceptor/interceptor-helper.service.spec.ts
@@ -1,11 +1,11 @@
 import { inject, TestBed } from '@angular/core/testing';
+
 import { InterceptorHelperService } from './interceptor-helper.service';
 
-
-let intHelpService: InterceptorHelperService;
-const sampleUrl = 'test-url-with-random-vals-9fj-df-3fdD-fdfFf3';
-
 describe('InterceptorHelperService', () => {
+  const sampleUrl = 'test-url-with-random-values-9fj-df-3fdD-fdfFf3';
+  let intHelpService: InterceptorHelperService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [InterceptorHelperService]
@@ -19,7 +19,7 @@ describe('InterceptorHelperService', () => {
 
   it('should (for `isExcludedFromErrorHandling`) return `true` if the argument is inside the private var ' +
     '`excludedFromErrorHandling`, `false` otherwise', () => {
-      (<string[]>intHelpService['excludedFromErrorHandling']).push(sampleUrl);
+      intHelpService['excludedFromErrorHandling'].push(sampleUrl);
       expect(intHelpService.isExcludedFromErrorHandling(sampleUrl)).toBeTruthy();
       expect(intHelpService.isExcludedFromErrorHandling('another-sample')).toBeFalsy();
     });
@@ -27,7 +27,7 @@ describe('InterceptorHelperService', () => {
   it('should (for `addExcludedFromErrorHandling`) add the argument inside the private var ' +
     '`excludedFromErrorHandling` if the url was not already there (returns `true`, `false` otherwise)', () => {
       expect(intHelpService.addExcludedFromErrorHandling(sampleUrl)).toBeTruthy();
-      expect((<string[]>intHelpService['excludedFromErrorHandling']).indexOf(sampleUrl) !== -1).toBeTruthy();
+      expect(intHelpService['excludedFromErrorHandling']).toContain(sampleUrl);
       // url already there
       expect(intHelpService.addExcludedFromErrorHandling(sampleUrl)).toBeFalsy();
     });
@@ -35,12 +35,12 @@ describe('InterceptorHelperService', () => {
   it('should (for `removeExcludedFromErrorHandling`) remove the argument inside the private var ' +
     '`excludedFromErrorHandling`(returns `true` if the args was already there, `false` otherwise', () => {
       const url = 'another-sample';
-      (<string[]>intHelpService['excludedFromErrorHandling']).push(sampleUrl);
+      intHelpService['excludedFromErrorHandling'].push(sampleUrl);
       // url is not in array
-      expect((<string[]>intHelpService['excludedFromErrorHandling']).indexOf(url) !== -1).toBeFalsy();
+      expect(intHelpService['excludedFromErrorHandling']).not.toContain(url);
       expect(intHelpService.removeExcludedFromErrorHandling(url)).toBeFalsy();
       /// sampleUrl is in array
-      expect((<string[]>intHelpService['excludedFromErrorHandling']).indexOf(sampleUrl) !== -1).toBeTruthy();
+      expect(intHelpService['excludedFromErrorHandling']).toContain(sampleUrl);
       expect(intHelpService.removeExcludedFromErrorHandling(sampleUrl)).toBeTruthy();
     });
 });
