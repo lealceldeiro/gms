d4ca6a21751d03d115019a105e9a0a6ac96d76f8
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2019-10-06 14:37:09 +0300

    Client: Update Unit Test
    - Refactor unit tests
    - Refactor import indentation

--- client/src/app/home/home.component.spec.ts
+++ client/src/app/home/home.component.spec.ts
@@ -2,31 +2,30 @@ import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { BehaviorSubject, Subscription } from 'rxjs';
+
 import { SessionService } from '../core/session/session.service';
 import { HomeComponent } from './home.component';
 import { AppConfig } from '../core/config/app.config';
 import { MockAppConfig } from '../shared/test-util/mock/app.config';
 
 describe('HomeComponent', () => {
+  const behaviorSubject = new BehaviorSubject<boolean>(false);
   let component: HomeComponent;
   let componentDe: DebugElement;
   let fixture: ComponentFixture<HomeComponent>;
-  const behaviorSubject = new BehaviorSubject<boolean>(false);
   let subscription: Subscription;
-  const spy = { isLoggedInSpyFn: () => { } };
-  let spyIsLoggedIn: jasmine.Spy;
-
-  const sessionServiceStub = {
-    isLoggedIn: () => { spy.isLoggedInSpyFn(); return behaviorSubject.asObservable(); }
-  };
+  let sessionServiceSpy: jasmine.SpyObj<SessionService>;
 
   beforeEach(async(() => {
+    sessionServiceSpy = jasmine.createSpyObj('SessionService', ['isLoggedIn']);
+    sessionServiceSpy.isLoggedIn.and.returnValue(behaviorSubject.asObservable());
+
     TestBed.configureTestingModule({
       declarations: [HomeComponent],
       schemas: [NO_ERRORS_SCHEMA],
       providers: [
         AppConfig,
-        { provide: SessionService, useValue: sessionServiceStub }
+        { provide: SessionService, useValue: sessionServiceSpy }
       ]
     }).compileComponents();
   }));
@@ -38,7 +37,6 @@ describe('HomeComponent', () => {
     component = fixture.componentInstance;
     componentDe = fixture.debugElement;
     fixture.detectChanges();
-    spyIsLoggedIn = spyOn(spy, 'isLoggedInSpyFn');
   });
 
   afterEach(() => {
@@ -54,11 +52,11 @@ describe('HomeComponent', () => {
 
   it('should call SessionService#isLoggedIn on init', () => {
     component.ngOnInit();
-    expect(spyIsLoggedIn).toHaveBeenCalledTimes(1);
+    expect(sessionServiceSpy.isLoggedIn).toHaveBeenCalled();
   });
 
   it('should not render jumbotron if the user is logged in', () => {
-    subscription = sessionServiceStub.isLoggedIn().subscribe((li) => {
+    subscription = sessionServiceSpy.isLoggedIn().subscribe((li) => {
       if (li) {
         fixture.detectChanges();
         expect(componentDe.query(By.css('#jmb-panel'))).toBeNull();
@@ -68,7 +66,7 @@ describe('HomeComponent', () => {
   });
 
   it('should render jumbotron if the user is not logged in', () => {
-    subscription = sessionServiceStub.isLoggedIn().subscribe((li) => {
+    subscription = sessionServiceSpy.isLoggedIn().subscribe((li) => {
       if (!li) {
         fixture.detectChanges();
         expect(componentDe.query(By.css('#jmb-panel'))).not.toBeNull('Jumbotron panel should not be null');
--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+
 import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
