23711569914a4358198b2e93ffbb1d7fa1b876d9
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2019-10-06 20:03:25 +0300

    Client: Update Unit Test
    - Refactor unit tests

--- client/src/app/permissions/permission-info/permission-info.component.spec.ts
+++ client/src/app/permissions/permission-info/permission-info.component.spec.ts
@@ -1,9 +1,9 @@
 import { Location } from '@angular/common';
 import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 
 import { Role } from '../../roles/shared/role.model';
 import { RolePd } from '../../roles/shared/role.pd';
@@ -14,9 +14,6 @@ import { PermissionService } from '../shared/permission.service';
 import { PermissionInfoComponent } from './permission-info.component';
 
 describe('PermissionInfoComponent', () => {
-
-  let component: PermissionInfoComponent;
-  let fixture: ComponentFixture<PermissionInfoComponent>;
   const fakeRoles: Array<Role> = [
     new Role(),
     {
@@ -36,42 +33,40 @@ describe('PermissionInfoComponent', () => {
       totalPages: 0
     }
   } as RolePd).asObservable();
-  const spy = {
-    location: { back: () => { } },
-    permission: {
-      getInfo: (_id: any) => { },
-      getRoles: (_id: any, _size: any, _page: any) => { }
-    }
-  };
-  const permissionServiceStub = {
-    getPermissionInfo: (_id: number): Observable<Permission> => {
-      spy.permission.getInfo(_id);
-      return new BehaviorSubject<Permission>(new Permission()).asObservable();
-    },
-    getPermissionRoles: (_id: number, _size: number, _page: number) => {
-      spy.permission.getRoles(_id, _size, _page);
-      return fakeRolesResult;
-    }
-  };
-  const locationStub = { back: () => { spy.location.back(); } };
   const id = getRandomNumber();
-  let idGetter = (): any => id;
-  const activatedRouteStub = { snapshot: { paramMap: { get: (): any => idGetter() } } };
+
+  let component: PermissionInfoComponent;
+  let fixture: ComponentFixture<PermissionInfoComponent>;
+  let permissionServiceSpy: jasmine.SpyObj<PermissionService>;
+
+  let locationSpy: jasmine.SpyObj<Location>;
+  let paramMapSpy: jasmine.SpyObj<ParamMap>;
+  let activatedRouteStub: ActivatedRoute;
 
   beforeEach(async(() => {
+    permissionServiceSpy = jasmine.createSpyObj('PermissionService', ['getPermissionInfo', 'getPermissionRoles']);
+    permissionServiceSpy.getPermissionInfo.and.returnValue(of(new Permission()));
+    permissionServiceSpy.getPermissionRoles.and.returnValue(fakeRolesResult);
+
+    locationSpy = jasmine.createSpyObj('Location', ['back']);
+
+    paramMapSpy = jasmine.createSpyObj('paramMapSpy', ['get']);
+    paramMapSpy.get.and.returnValue(id.toString());
+
+    activatedRouteStub = (<unknown>{ snapshot: { paramMap: paramMapSpy } }) as ActivatedRoute;
+
     TestBed.configureTestingModule({
       imports: [SharedModule],
       declarations: [PermissionInfoComponent],
       providers: [
-        { provide: PermissionService, useValue: permissionServiceStub },
-        { provide: Location, useValue: locationStub },
+        { provide: PermissionService, useValue: permissionServiceSpy },
+        { provide: Location, useValue: locationSpy },
         { provide: ActivatedRoute, useValue: activatedRouteStub }
       ]
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    idGetter = (): any => id;
     fixture = TestBed.createComponent(PermissionInfoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -82,25 +77,22 @@ describe('PermissionInfoComponent', () => {
   });
 
   it('should call PermissionService#getPermissionInfo on init', () => {
-    const getPermissionInfoSpy = spyOn(spy.permission, 'getInfo');
-    component.ngOnInit();
-    expect(getPermissionInfoSpy).toHaveBeenCalledTimes(1);
-    expect(getPermissionInfoSpy).toHaveBeenCalledWith(id);
+    // ngOnInit called by component creation in beforeEach
+    expect(permissionServiceSpy.getPermissionInfo).toHaveBeenCalledTimes(1);
+    expect(permissionServiceSpy.getPermissionInfo).toHaveBeenCalledWith(id);
   });
 
   it('should not call PermissionService#getPermissionInfo, but Location#back if the id is an invalid value', () => {
-    const getPermissionInfoSpy = spyOn(spy.permission, 'getInfo');
-    const locationBackSpy = spyOn(spy.location, 'back');
-    idGetter = (): any => 'someInvalidId';
+    permissionServiceSpy.getPermissionInfo.calls.reset();
+    paramMapSpy.get.and.returnValue(null);
     component['getPermissionInfo']();
-    expect(getPermissionInfoSpy).not.toHaveBeenCalled();
-    expect(locationBackSpy).toHaveBeenCalledTimes(1);
+    expect(permissionServiceSpy.getPermissionInfo).not.toHaveBeenCalled();
+    expect(locationSpy.back).toHaveBeenCalledTimes(1);
   });
 
   it('should call Location#back on call to back function', () => {
-    const locationBackSpy = spyOn(spy.location, 'back');
     component['back']();
-    expect(locationBackSpy).toHaveBeenCalledTimes(1);
+    expect(locationSpy.back).toHaveBeenCalledTimes(1);
   });
 
   it('should show a text to allow retrieving the roles using the selected permission and hide the table of roles', () => {
@@ -111,15 +103,14 @@ describe('PermissionInfoComponent', () => {
   });
 
   it('should hide the text to allow retrieving the roles using the selected permission and show the table of roles', fakeAsync(() => {
-    const getPermissionRolesSpy = spyOn(spy.permission, 'getRoles');
     const size = getRandomNumber();
     const page = getRandomNumber();
     component.page.size = size;
     component.showInRoles(page);
     fixture.detectChanges();
     tick();
-    expect(getPermissionRolesSpy).toHaveBeenCalledTimes(1);
-    expect(getPermissionRolesSpy).toHaveBeenCalledWith(id, size, page);
+    expect(permissionServiceSpy.getPermissionRoles).toHaveBeenCalledTimes(1);
+    expect(permissionServiceSpy.getPermissionRoles).toHaveBeenCalledWith(id, size, page);
     expect(component.rolesLoaded).toEqual(true);
     expect(component.roles).toEqual(fakeRoles);
     expect(fixture.debugElement.query(By.css('#showRolesLnk'))).toBeFalsy();
@@ -127,15 +118,13 @@ describe('PermissionInfoComponent', () => {
   }));
 
   it('should use `this.page.current` as default if not page is provided to `#showInPermissions`', () => {
-    const getRolePermissionsSpy = spyOn(spy.permission, 'getRoles');
     const size = getRandomNumber();
     const page = getRandomNumber();
     component.page.current = page;
     component.page.size = size;
     component.showInRoles();
     fixture.detectChanges();
-    expect(getRolePermissionsSpy).toHaveBeenCalledTimes(1);
-    expect(getRolePermissionsSpy).toHaveBeenCalledWith(id, size, page);
+    expect(permissionServiceSpy.getPermissionRoles).toHaveBeenCalledTimes(1);
+    expect(permissionServiceSpy.getPermissionRoles).toHaveBeenCalledWith(id, size, page);
   });
-
 });
