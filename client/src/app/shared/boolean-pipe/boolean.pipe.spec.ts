d7ffe0a1ecc69c20cc1830a5c623ba8b896ed9bd
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2019-10-06 22:40:26 +0300

    Client: Update Unit Test
    - Refactor unit tests

--- client/src/app/shared/boolean-pipe/boolean.pipe.spec.ts
+++ client/src/app/shared/boolean-pipe/boolean.pipe.spec.ts
@@ -1,31 +1,31 @@
+import { PipeTransform } from '@angular/core';
+
 import { BooleanPipe } from './boolean.pipe';
 import { getRandomNumber } from '../test-util/functions.util';
 
 describe('BooleanPipe', () => {
+  let pipe: PipeTransform;
+
+  beforeEach(() => pipe = new BooleanPipe());
 
   it('create an instance', () => {
-    const pipe = new BooleanPipe();
     expect(pipe).toBeTruthy();
   });
 
   it('should return true when no value is specified as first argument - true case', () => {
-    const pipe = new BooleanPipe();
     expect(pipe.transform(true)).toEqual('true');
   });
 
   it('should return false when no value is specified as first argument - false case', () => {
-    const pipe = new BooleanPipe();
     expect(pipe.transform(false)).toEqual('false');
   });
 
   it('should return a custom value when specified as first argument - true case', () => {
-    const pipe = new BooleanPipe();
     const trueVal = 'val-' + getRandomNumber() + 'rnd';
     expect(pipe.transform(true, trueVal)).toEqual(trueVal);
   });
 
   it('should return a custom value when specified as second argument - false case', () => {
-    const pipe = new BooleanPipe();
     const falsyVal = 'val-' + getRandomNumber() + 'rnd';
     expect(pipe.transform(false, undefined, falsyVal)).toEqual(falsyVal);
   });
